d2c649dba8d38f28e9e9c518be099b83b3eb8edd
asibulh <asibuldip@gmail.com>
2026-03-05 23:06:20 +0600

    added module_1

--- module_2.py
+++ module_2.py
@@ -6,3 +6,12 @@ import math
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * radius * radius
 print("The area of the circle is:", area)
+
+
+
+length = float(input("Enter the length of the circle: "))
+width = float(input("Enter the width of the circle: "))
+area = length * width
+perimeter = (length + width) * 2
+print("The area of the circle is:", area)
+print("The perimeter of the circle is:", perimeter)
