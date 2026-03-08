eb02180143d171046242e46a20629afdc1aacb6f
asibulh <asibuldip@gmail.com>
2026-03-08 16:40:11 +0600

    added module_3

--- test1.py
+++ test1.py
@@ -6,3 +6,11 @@ else:
     remaining_years = 18 - age
     print("You are not old enough to vote.")
     print("You can vote after", remaining_years, "year(s).")
+
+    age = int(input("enter your age:"))
+    if age >= 18:
+        print("you are old enough to vote in Finnish parliamentary elections.")
+    else:
+        remaining_years = age - 18
+        print("you are not old enough to vote in Finnish parliamentary elections.")
+        print("you can vote after", remaining_years, "year(s)")
