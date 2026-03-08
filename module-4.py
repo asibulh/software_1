a1c6768f7d8b92d5cf0d58078b61dbb83741f9a6
asibulh <asibuldip@gmail.com>
2026-03-08 23:44:57 +0600

    added module_4

--- module-4.py
+++ module-4.py
@@ -1,1 +1,15 @@
-print("Hello World")
+number = 1
+while number <= 1000:
+ if number % 3 == 0:
+    print(number)
+ number += 1
+
+
+
+ while True:
+     inches = float(input("Enter inches: "))
+ if inches < 0:
+     print("Program ended.")
+     break
+ cm = inches * 2.54
+ print(cm, "centimeters")
