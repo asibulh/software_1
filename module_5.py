b190061f6be598d866c8bbc8286279cc003f0fb6
asibulh <asibuldip@gmail.com>
2026-03-11 01:40:53 +0600

    added module_5

--- module_5.py
+++ module_5.py
@@ -8,3 +8,37 @@ print("Sum of the dice:", total)
 
 
 
+numbers = []
+num = input("Enter a number (empty to quit): ")
+while num != "":
+    numbers.append(float(num))
+    num = input("Enter a number (empty to quit): ")
+numbers.sort(reverse=True)
+print("Five greatest numbers:")
+i =0
+while i < 5 and i < len(numbers):
+    print(numbers[i])
+    i=i+1
+
+
+
+num = int(input("Enter an integer: "))
+is_prime = True
+for i in range(2, num):
+    if num % i == 0:
+        is_prime = False
+        break
+if num >1 and is_prime:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
+
+
+
+cities = []
+for i in range(5):
+    name=input("Enter city name: ")
+    cities.append(name)
+    print("Cities you entered:")
+for city in cities:
+    print(city)
--- projet1.py
+++ projet1.py
@@ -1,7 +1,7 @@
-age= int(input("enter your age:"))
-if age>=18:
-    print( "you are eligible to vote in Finnish parliamentary elections.")
-else:
-    remaining_years=age-18
-    print( "you are not old enough to vote in Finnish parliamentary elections.")
-    print("You can vote after", remaining_years, "years ago.")
+cities = []
+for i in range(5):
+    name=input("Enter city name: ")
+    cities.append(name)
+    print("Cities you entered:")
+for city in cities:
+    print(city)
