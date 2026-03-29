eade72aa76318632c244081255bc3cf67978a348
asibulh <asibuldip@gmail.com>
2026-03-29 14:22:09 +0600

    added_exercise

---
+++ module_10.py
@@ -0,0 +1,202 @@
+#1
+
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self):
+        if self.current_floor < self.top_floor:
+            self.current_floor += 1
+            print(f"Elevator is now at floor {self.current_floor}")
+
+    def floor_down(self):
+        if self.current_floor > self.bottom_floor:
+            self.current_floor -= 1
+            print(f"Elevator is now at floor {self.current_floor}")
+
+    def go_to_floor(self, target_floor):
+        while self.current_floor < target_floor:
+            self.floor_up()
+        while self.current_floor > target_floor:
+            self.floor_down()
+
+# Main program
+
+elevator = Elevator(1, 10)
+
+print("Going to floor 5:")
+elevator.go_to_floor(5)
+
+print("Returning to bottom floor:")
+elevator.go_to_floor(1)
+
+
+#2
+
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self):
+        if self.current_floor < self.top_floor:
+            self.current_floor += 1
+            print(f"Elevator at floor {self.current_floor}")
+
+    def floor_down(self):
+        if self.current_floor > self.bottom_floor:
+            self.current_floor -= 1
+            print(f"Elevator at floor {self.current_floor}")
+
+    def go_to_floor(self, target_floor):
+        while self.current_floor < target_floor:
+            self.floor_up()
+        while self.current_floor > target_floor:
+            self.floor_down()
+
+
+class Building:
+    def __init__(self, bottom_floor, top_floor, num_elevators):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.elevators = []
+
+        for i in range(num_elevators):
+            self.elevators.append(Elevator(bottom_floor, top_floor))
+
+    def run_elevator(self, elevator_number, target_floor):
+        print(f"\nRunning elevator {elevator_number} to floor {target_floor}")
+        self.elevators[elevator_number].go_to_floor(target_floor)
+
+# Main program
+
+building = Building(1, 10, 3)
+
+building.run_elevator(0, 7)
+building.run_elevator(1, 3)
+
+
+#3
+
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self):
+        if self.current_floor < self.top_floor:
+            self.current_floor += 1
+            print(f"Elevator at floor {self.current_floor}")
+
+    def floor_down(self):
+        if self.current_floor > self.bottom_floor:
+            self.current_floor -= 1
+            print(f"Elevator at floor {self.current_floor}")
+
+    def go_to_floor(self, target_floor):
+        while self.current_floor < target_floor:
+            self.floor_up()
+        while self.current_floor > target_floor:
+            self.floor_down()
+
+
+class Building:
+    def __init__(self, bottom_floor, top_floor, num_elevators):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.elevators = []
+
+        for i in range(num_elevators):
+            self.elevators.append(Elevator(bottom_floor, top_floor))
+
+    def run_elevator(self, elevator_number, target_floor):
+        self.elevators[elevator_number].go_to_floor(target_floor)
+
+    def fire_alarm(self):
+        print("\n FIRE ALARM! Moving all elevators to bottom floor....")
+        for elevator in self.elevators:
+            elevator.go_to_floor(self.bottom_floor)
+
+# Main program
+
+building = Building(1, 10, 3)
+
+building.run_elevator(0, 8)
+building.run_elevator(1, 5)
+
+building.fire_alarm()
+
+
+#4
+
+import random
+
+class Car:
+    def __init__(self, reg, max_speed):
+        self.registration_number = reg
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, change):
+        self.current_speed += change
+
+        if self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+
+class Race:
+    def __init__(self, name, distance, cars):
+        self.name = name
+        self.distance = distance
+        self.cars = cars
+
+    def hour_passes(self):
+        for car in self.cars:
+            change = random.randint(-10, 15)
+            car.accelerate(change)
+            car.drive(1)
+
+    def print_status(self):
+        print(f"\nRace: {self.name}")
+        print(f"{'Reg No':<10} {'Speed':<10} {'Distance':<15}")
+        print("-" * 40)
+
+        for car in self.cars:
+            print(f"{car.registration_number:<10} "
+                  f"{car.current_speed:<10} "
+                  f"{car.travelled_distance:<15.2f}")
+
+    def race_finished(self):
+        for car in self.cars:
+            if car.travelled_distance >= self.distance:
+                return True
+        return False
+
+# Main program
+cars = []
+for i in range(1, 11):
+    cars.append(Car(f"ABC-{i}", random.randint(100, 200)))
+
+race = Race("Grand Demolition Derby", 8000, cars)
+
+hours = 0
+
+while not race.race_finished():
+    race.hour_passes()
+    hours += 1
+
+    if hours % 10 == 0:
+        race.print_status()
+
+print("\n Final Result")
+race.print_status()
