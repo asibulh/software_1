3c3eb0a92f45ee7e9720f562e3b9fb084a6c5b71
asibulh <asibuldip@gmail.com>
2026-04-05 17:29:32 +0600

    add_my_exercise

--- projet1.py
+++ projet1.py
@@ -1,7 +1,27 @@
-cities = []
-for i in range(5):
-    name=input("Enter city name: ")
-    cities.append(name)
-    print("Cities you entered:")
-for city in cities:
-    print(city)
+import pymysql
+
+connection = pymysql.connect(
+    host="127.0.0.1",
+    user="root",
+    password="166082",
+    database="flight_game",
+    port=3306
+)
+
+cursor = connection.cursor()
+
+icao = input("Enter ICAO code: ").upper().strip()
+
+query = "SELECT name, municipality FROM airport WHERE ident=%s"
+cursor.execute(query, (icao,))
+
+result = cursor.fetchone()
+
+if result:
+    print("Airport:", result[0])
+    print("City:", result[1])
+else:
+    print("Not found")
+
+cursor.close()
+connection.close()
